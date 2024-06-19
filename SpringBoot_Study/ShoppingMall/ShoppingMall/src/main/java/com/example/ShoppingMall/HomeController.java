0276d79ca29dea29b5287caf45b72d7b4ccd7edb
cemzzz <koreaok01@gmail.com>
2024-06-19 19:01:44 +0900

    프로젝트 생성

--- SpringBoot_Study/ShoppingMall/ShoppingMall/src/main/java/com/example/ShoppingMall/HomeController.java
+++ SpringBoot_Study/ShoppingMall/ShoppingMall/src/main/java/com/example/ShoppingMall/HomeController.java
@@ -1,4 +1,12 @@
 package com.example.ShoppingMall;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class HomeController {
+    @GetMapping("/")
+    String home(){
+        return "home.html";
+    }
 }
